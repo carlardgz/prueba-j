785ae22f42ecf2f4ab7a699e532c5df9f08e2d88
Hernán Adalid <escalera_perez@ucol.mx>
2022-11-28 14:19:46 -0600

    cambio de prueba

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "CARLA RODRÍGUEZ AGUILAR, Facultad de Telemática, 28 de noviembre de 2022";
+echo "Carla Rodríguez Aguilar. Facultad de Telemática. 28 de noviembre de 2022.";
 ?>
 
 </body>
