086ee0de0862b30320cce89cca1f22045a5197c8
itzelmun <isanchez17@ucol.mx>
2022-12-06 18:05:15 +0000

    :)

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "HOLA. Carla Rodríguez Aguilar. Facultad de Telemática. 30 de noviembre de 2022.";
+echo "HOLA. :) Carla Rodríguez Aguilar. Facultad de Telemática. 30 de noviembre de 2022.";
 ?>
 
 </body>
