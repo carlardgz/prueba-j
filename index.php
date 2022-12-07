8edec60566df44a09f2eb5936128bd88d173218b
itzelmun <isanchez17@ucol.mx>
2022-12-07 20:42:42 +0000

    feliz navidad

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "HOLA. :) Carla Rodríguez Aguilar. Facultad de Telemática. 7 de diciembre de 2022.";
+echo "HOLA. :) Carla Rodríguez Aguilar. Facultad de Telemática. 7 de diciembre de 2022. Feliz Navidad. :D";
 ?>
 
 </body>
