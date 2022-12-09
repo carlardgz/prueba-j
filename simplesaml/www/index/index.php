f00445fed9d690128dbfbf7f4d8854751fc7e9bf
itzelmun <isanchez17@ucol.mx>
2022-12-09 22:31:51 +0000

    yaxfavor

--- simplesaml/www/index/index.php
+++ simplesaml/www/index/index.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "¿hola? k está pasando TODO BIEN AL CIEN!!HOLA. :) Carla Rodríguez Aguilar. Facultad de Telemática. 9 de diciembre de 2022. Feliz Navidad. :D Gatito bonito <3 ";
+echo "Estoy harta :D ¿hola? k está pasando TODO BIEN AL CIEN!!HOLA. :) Carla Rodríguez Aguilar. Facultad de Telemática. 9 de diciembre de 2022. Feliz Navidad. :D Gatito bonito <3 ";
 ?>
 
 </body>
