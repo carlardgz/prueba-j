763a64ab5b5e8d75af9e9f2f3e1d9daf82404f15
itzelmun <isanchez17@ucol.mx>
2022-12-09 20:43:05 +0000

    ayuda

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "¿hola? TODO BIEN AL CIEN!!HOLA. :) Carla Rodríguez Aguilar. Facultad de Telemática. 9 de diciembre de 2022. Feliz Navidad. :D Gatito bonito <3 ";
+echo "¿hola? k está pasando TODO BIEN AL CIEN!!HOLA. :) Carla Rodríguez Aguilar. Facultad de Telemática. 9 de diciembre de 2022. Feliz Navidad. :D Gatito bonito <3 ";
 ?>
 
 </body>
