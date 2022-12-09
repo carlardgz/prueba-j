a40420bcce14a47436883d643c60e8e09a6a9763
itzelmun <isanchez17@ucol.mx>
2022-12-09 17:48:56 +0000

    gato

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "HOLA. :) Carla Rodríguez Aguilar. Facultad de Telemática. 8 de diciembre de 2022. Feliz Navidad. :D";
+echo "HOLA. :) Carla Rodríguez Aguilar. Facultad de Telemática. 9 de diciembre de 2022. Feliz Navidad. :D Gatito bonito <3 ";
 ?>
 
 </body>
